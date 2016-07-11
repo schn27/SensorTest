f9daac6e8e69a0386943e6ee594a42d11ff2f856
Aleksandr Malikov <schn27@gmail.com>
2016-07-11 09:40:50 +0300

    update

--- app/src/main/java/luft27/sensortest/MainActivity.java
+++ app/src/main/java/luft27/sensortest/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
 				(TextView) findViewById(R.id.gyroX),
 				(TextView) findViewById(R.id.gyroY),
 				(TextView) findViewById(R.id.gyroZ)
+		};
+
+		accel = new TextView[]{
+				(TextView) findViewById(R.id.accelX),
+				(TextView) findViewById(R.id.accelY),
+				(TextView) findViewById(R.id.accelZ),
+				(TextView) findViewById(R.id.accelModule)
+		};
+
+		mag = new TextView[]{
+				(TextView) findViewById(R.id.magX),
+				(TextView) findViewById(R.id.magY),
+				(TextView) findViewById(R.id.magZ),
+				(TextView) findViewById(R.id.magModule)
 		};
 
 		angles = new TextView[]{
@@ -53,34 +67,79 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	private void registerSensorListener() {
-		Sensor sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		Sensor sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		Sensor sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Sensor sensorMag = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
 		if (sensorGyro != null) {
-			sensorManager.registerListener(sensorListener, sensorGyro, 100 * 1000);
+			sensorManager.registerListener(this, sensorGyro, 100 * 1000);
 		} else {
 			for (TextView t : gyro) {
+				t.setText("N/A");
+			}
+		}
+
+		if (sensorAccel != null) {
+			sensorManager.registerListener(this, sensorAccel, 100 * 1000);
+		} else {
+			for (TextView t : accel) {
+				t.setText("N/A");
+			}
+		}
+
+		if (sensorMag != null) {
+			sensorManager.registerListener(this, sensorMag, 100 * 1000);
+		} else {
+			for (TextView t : mag) {
 				t.setText("N/A");
 			}
 		}
 	}
 
 	private void unregisterSensorListener() {
-		sensorManager.unregisterListener(sensorListener);
+		sensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		double total;
+
+		switch (sensorEvent.sensor.getType()) {
+			case Sensor.TYPE_GYROSCOPE:
+				for (int i = 0; i < 3; ++i) {
+					gyro[i].setText(String.format("%.2f", sensorEvent.values[i]));
+				}
+				break;
+
+			case Sensor.TYPE_ACCELEROMETER:
+				total = 0;
+				for (int i = 0; i < 3; ++i) {
+					accel[i].setText(String.format("%.2f", sensorEvent.values[i]));
+					total += sensorEvent.values[i] * sensorEvent.values[i];
+				}
+
+				accel[3].setText(String.format("%.2f", Math.sqrt(total)));
+				break;
+
+			case Sensor.TYPE_MAGNETIC_FIELD:
+				total = 0;
+				for (int i = 0; i < 3; ++i) {
+					mag[i].setText(String.format("%.2f", sensorEvent.values[i]));
+					total += sensorEvent.values[i] * sensorEvent.values[i];
+				}
+
+				mag[3].setText(String.format("%.2f", Math.sqrt(total)));
+				break;
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int i) {
 	}
 
 	private TextView[] gyro;
+	private TextView[] accel;
+	private TextView[] mag;
 	private TextView[] angles;
 	private SensorManager sensorManager;
-
-	private SensorEventListener sensorListener = new SensorEventListener() {
-		@Override
-		public void onSensorChanged(SensorEvent sensorEvent) {
-			for (int i = 0; i < gyro.length; ++i) {
-				gyro[i].setText(String.format("%.2f", sensorEvent.values[i]));
-			}
-		}
-
-		@Override
-		public void onAccuracyChanged(Sensor sensor, int i) {
-		}
-	};
 }
